e93c6bc0da97622168cab7222c42bb17d427378b
dzca <dzca>
2023-02-26 17:15:11 -0500

    add island problems - BFS

--- src/main/kotlin/gurus/islands.kt
+++ src/main/kotlin/gurus/islands.kt
@@ -4,7 +4,7 @@ package gurus
  * questions to find islands
  */
 
-fun countIslands(a: Array<Array<Int>>): Int {
+fun countIslandsDFS(a: Array<Array<Int>>): Int {
     val c = a.size -1
     val r = a[0].size -1
     var x = 0 // count of islands
@@ -33,4 +33,52 @@ fun findIslandDFS(a:Array<Array<Int>>,i: Int, j:Int){
     findIslandDFS(a, i+1, j)
     findIslandDFS(a, i, j+1)
     findIslandDFS(a, i, j-1)
+}
+
+fun countIslandsBFS(a: Array<Array<Int>>): Int{
+    val c = a.size -1
+    val r = a[0].size -1
+
+    var x = 0
+    for(i in 0..c){
+        for(j in 0..r){
+            if(a[i][j] == 1){
+                x++
+                println("add bfs $i, $j")
+                findIslandBFS(a, i, j)
+            }
+        }
+    }
+    return x
+}
+
+fun findIslandBFS(a: Array<Array<Int>>, x:Int, y:Int){
+    println("bfs push $x, $y")
+    val q = ArrayDeque<Array<Int>>()
+    q.addLast(arrayOf(x, y))
+
+
+    while(q.size > 0){
+        println("q size= ${q.size}")
+        val x = q.removeFirst()
+        val i = x[0]
+        val j = x[1]
+        println("bfs pop $i, $j, ${a[0].size}")
+
+        if(i< 0 || i >= a.size || j <0 || j>=a[0].size){
+            continue
+        }
+
+        if(a[i][j] == 1){
+            println("bfs found $i, $j, q.size=${q.size}")
+            a[i][j] = 0
+        } else {
+            continue
+        }
+        q.addLast(arrayOf(i-1,j))
+        q.addLast(arrayOf(i+1,j))
+        q.addLast(arrayOf(i,j-1))
+        q.addLast(arrayOf(i,j+1))
+    }
+    println("exit while")
 }
--- src/test/kotlin/gurus/t3-islands.kt
+++ src/test/kotlin/gurus/t3-islands.kt
@@ -6,7 +6,7 @@ import kotlin.test.assertEquals
 class TestIslandsProblems {
 
     @Test
-    fun `count islands`(){
+    fun `count islands DFS`(){
         val a1 = arrayOf(
             arrayOf(0, 1, 1, 1, 0),
             arrayOf(0, 0, 0, 1, 1),
@@ -23,7 +23,29 @@ class TestIslandsProblems {
             arrayOf(0, 0, 1, 0, 0)
         )
 
-        assertEquals(1, countIslands(a1))
-        assertEquals(3, countIslands(a2))
+        assertEquals(1, countIslandsDFS(a1))
+        assertEquals(3, countIslandsDFS(a2))
+    }
+
+    @Test
+    fun `count islands BFS`(){
+        val a1 = arrayOf(
+            arrayOf(0, 1, 1, 1, 0),
+            arrayOf(0, 0, 0, 1, 1),
+            arrayOf(0, 1, 1, 1, 0),
+            arrayOf(0, 1, 1, 0, 0),
+            arrayOf(0, 0, 0, 0, 0)
+        )
+
+        val a2 = arrayOf(
+            arrayOf(1, 1, 1, 0, 0),
+            arrayOf(0, 1, 0, 0, 1),
+            arrayOf(0, 0, 1, 1, 0),
+            arrayOf(0, 0, 1, 0, 0 ),
+            arrayOf(0, 0, 1, 0, 0)
+        )
+
+        assertEquals(1, countIslandsBFS(a1))
+        assertEquals(3, countIslandsBFS(a2))
     }
 }
